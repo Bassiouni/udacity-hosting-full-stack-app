1f1d3a09fc9bb3730615d51c67f1577cd3f8b442
Mohammed Basyouni <mohmah345@gmail.com>
2022-04-25 22:14:44 +0200

    x

--- reactnd-contacts-complete/src/utils/ContactsAPI.js
+++ reactnd-contacts-complete/src/utils/ContactsAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'https://Test3-env.eba-am32hwsk.us-east-2.elasticbeanstalk.com'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://reactnd-env.eba-9werppqp.us-east-2.elasticbeanstalk.com'
 
 let token = localStorage.token
 
@@ -13,11 +13,12 @@ const headers = {
 export const getAll = () =>
   fetch(`${api}/contacts`, { headers })
     .then(res => res.json())
+    .then(data => data)
 
 export const remove = (contact) =>
   fetch(`${api}/contacts/${contact.id}`, { method: 'DELETE', headers })
     .then(res => res.json())
-
+    .then(data => data.contact)
 
 export const create = (body) =>
   fetch(`${api}/contacts`, {
--- reactnd-contacts-server/config.js
+++ reactnd-contacts-server/config.js
@@ -1,2 +1,2 @@
 exports.port = process.env.PORT || 4000
-exports.origin = process.env.ORIGIN || `https://Test3-env.eba-am32hwsk.us-east-2.elasticbeanstalk.com:${exports.port}`
+exports.origin = process.env.ORIGIN || `http://localhost:${exports.port}`
